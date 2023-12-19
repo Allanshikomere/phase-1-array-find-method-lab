06b67070ef12c8123ca6d1be63ed4c2671aac598
Allanshikomere <allanshikomere@gmail.com>
2023-12-19 14:22:55 +0300

    Completed arrayFindMethodLab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    
+    const winningYearObject = record.find(obj => obj.result === "W");
+  
+    
+    return winningYearObject ? winningYearObject.year : undefined;
+  }
